a98b0591845103126592324a1512a0eb797bbc17
User <User@DESKTOP-519F49N>
2022-12-10 23:27:42 -0500

    Revert "semana13 listas de oredenamiento"

--- src/decimatercerasemana/MetodosDeOrdenamientoJugadores.java
+++ src/decimatercerasemana/MetodosDeOrdenamientoJugadores.java
@@ -49,14 +49,14 @@ public static void Rapido(int matrix[], int a, int b){
         while(matrix[to] > pivot){
             to--;
         }
-        if(from < = to){
+        if(from <=to){
             buf = matrix[from];
             matrix[from] = matrix[to];
             matrix[to] = buf;
             from++; to--;
         }
     }
-    while(from < = to); 
+    while(from <=to); 
     if(a < to){
         Rapido(matrix, a, to);
     }
